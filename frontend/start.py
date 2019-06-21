be8afaaf9636380bf4bc4c61a89db1fe0f14d9cd
t3santos <31711814+t3santos@users.noreply.github.com>
2019-06-21 19:55:54 -0300

    Update start.py

--- frontend/start.py
+++ frontend/start.py
@@ -1,19 +1,13 @@
 
 from flask import Flask, render_template
-from pymongo import MongoClient
+from lib import DadosAbertos
 
 app = Flask(__name__)
 
 
-# Conexao ao mongoDB
-conn = MongoClient('mongodb', 27017)
-
-# Conexao ao database
-banco = conn['projetoDep']
-
-# Conexao a tabela de banco de dados
 table = banco['deputados']
 
+tablegastos = banco['gastos']
 
 @app.route("/")
 def deputados():
@@ -27,7 +21,37 @@ def deputado(id):
    obj    = DadosAbertos()
    info   = obj.deputado_id(id)
    gastos = obj.deputado_despesas(id)
-   return render_template('gastos.html', listas=info, gastos=gastos)
+   datahoje = date.today()
+   meshoje = datahoje.month
+   valores = {}
+   meses = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho"]
+  
+  # Labels data
+   
+   line_months = []
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=8080)
+   for x in range(6):
+      line_months.append(meses[meshoje])
+      meshoje = meshoje - 1
+
+   line_months.reverse()
+   line_data = []
+   for gasto in gastos:
+       data = str(gasto['mes']) + '/' + str(gasto['ano'])
+     
+       
+
+       if data in valores:
+          line_data.append(gasto['valorLiquido']) 
+       else:
+          line_data.append(gasto['valorLiquido'])  
+
+   
+   line_labels = valores.keys()
+   line_values = valores.values()
+   line_data.append(random.randint(100,5000))
+   line_data.append(random.randint(100,5000)) 
+
+   return render_template('gastos.html', title='Grafico de Gastos', max=10000, labels=line_labels, values=line_data,
+   meses = line_months, x = random.randint(100,5000), y =
+   random.randint(100,5000))
