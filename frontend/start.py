88218790e66b2ac8a501a356168f7fd74688cd1a
t3santos <31711814+t3santos@users.noreply.github.com>
2019-06-21 19:15:29 -0300

    Update start.py

--- frontend/start.py
+++ frontend/start.py
@@ -1,14 +1,25 @@
 
 from flask import Flask, render_template
-from lib import DadosAbertos
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+
+# Conexao ao mongoDB
+conn = MongoClient('mongodb', 27017)
+
+# Conexao ao database
+banco = conn['projetoDep']
+
+# Conexao a tabela de banco de dados
+table = banco['deputados']
+
+
 @app.route("/")
 def deputados():
-   obj = DadosAbertos()
-   list_dep = obj.deputados()
-
+   list_dep = []
+   for dep in table.find():
+       list_dep.append(dep)
    return render_template('lista.html', listas=list_dep)
 
 @app.route("/gastos/<id>")
@@ -16,21 +27,7 @@ def deputado(id):
    obj    = DadosAbertos()
    info   = obj.deputado_id(id)
    gastos = obj.deputado_despesas(id)
-
-   valores = {}
-   for gasto in gastos:
-       data = str(gasto['mes']) + '/' + str(gasto['ano'])
-
-       if data in valores:
-          valores[data] += gasto['valorLiquido']    
-       else:
-          valores[data] = gasto['valorLiquido']   
-       
-   line_labels = valores.keys()
-   line_values = valores.values()
-
-   return render_template('gastos.html', title='Grafico de Gastos', max=10000, labels=line_labels, values=line_values)
-
+   return render_template('gastos.html', listas=info, gastos=gastos)
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', debug=True, port=8080)
+    app.run(host='0.0.0.0', debug=True, port=8080)
